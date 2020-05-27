5a5966d2f11035e9037f7ba89bb3832554dcf9f0
SMARTIUM <dev@smartium.io>
2020-05-27 13:53:47 -0300

    First COMMIT.

---
+++ Assets/_APP/Scripts/TextInput.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextInput : MonoBehaviour
+{
+    public Text nameOutput;
+    public InputField inputField;
+    public Camera camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Display.displays[0].SetParams(1920, 590, 0, 790);
+
+        if (Application.isEditor)
+        {
+            camera.name = "EDITOR";
+        }
+        else
+        {
+            camera.name = "InterAoVivo";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void sendText()
+    {
+        nameOutput.text = inputField.text;
+    }
+
+    public void resetTexts()
+    {
+        nameOutput.text = null;
+        inputField.text = null;
+    }
+}
